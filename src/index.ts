6273aea3c7d92b248fc8367358baf68eb583781b
Facundo Fernandez <facuassain@gmail.com>
2023-09-02 16:37:36 -0300

    json middleware added

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,14 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/servicio-1', (req, res) => {
-    res.send('Hello World!');
-    });
+    console.log(req.body);
+    res.json(req.body);
+});
+
 
 app.listen(3000, () => {
     console.log('\x1b[32mServer running on port 3000\x1b[0m');
-    });
+});
