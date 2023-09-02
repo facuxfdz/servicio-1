d07bcd692e7c3bede2fea026c5ef43f1e2cc26cf
javifgarcia <jagarca@frba.utn.edu.ar>
2023-09-02 17:57:20 -0300

    prueba push

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,9 @@
 import express from 'express';
 import Joi from 'joi';
-import { Comunidad } from './types/Comunidad';
+import Comunidad, { validateComunidad } from './types/Comunidad';
 
 const app = express();
 
-// Custom validation middlewares
-const comunidadSchema = Joi.object({
-    comunidades: Joi.array().items(Joi.object({
-        nombre: Joi.string().required(),
-        id: Joi.number().required()
-    })).required()
-});
-
-const validateComunidad = (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    const validationResult = comunidadSchema.validate(req.body);
-
-    if (validationResult.error) {
-        return res.status(400).json(validationResult.error);
-    }
-
-    const comunidades: Comunidad[] = req.body.comunidades;
-
-    const comunidadesParseadas: Comunidad[] = comunidades.map((comunidad) => ({
-        id: parseInt(comunidad.id.toString()),
-        nombre: comunidad.nombre
-    }));
-
-    req.body.comunidades = comunidadesParseadas;
-
-    next();
-};
-// End of custom validation middlewares
 
 // Express middlewares
 app.use(express.json());
--- src/types/Comunidad.ts
+++ src/types/Comunidad.ts
@@ -1,4 +1,45 @@
-export type Comunidad  = {
+import express from 'express';
+import Joi from 'joi';
+
+type Comunidad  = {
     id: number;
     nombre: string;
+    establecimientos: Establecimiento[];
 }
+
+type Establecimiento = {
+    id: string;
+}
+
+const comunidadSchema = Joi.object({
+    comunidades: Joi.array().items(Joi.object({
+        nombre: Joi.string().required(),
+        id: Joi.number().required(),
+        establecimientos: Joi.array().items(Joi.object({
+            id: Joi.string()
+        })).required()
+    })).required()
+});
+
+export const validateComunidad = (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    const validationResult = comunidadSchema.validate(req.body);
+
+    if (validationResult.error) {
+        return res.status(400).json(validationResult.error);
+    }
+
+    const comunidades: Comunidad[] = req.body.comunidades;
+
+    const comunidadesParseadas: Comunidad[] = comunidades.map((comunidad) => ({
+        id: parseInt(comunidad.id.toString()),
+        nombre: comunidad.nombre,
+        establecimientos: comunidad.establecimientos
+    }));
+
+    req.body.comunidades = comunidadesParseadas;
+
+    next();
+};
+// End of custom validation middlewares
+
+export default Comunidad
