14f93ab3e52743f7bd5107532d4635647886ccb5
Facundo Fernandez <facuassain@gmail.com>
2023-09-02 17:08:53 -0300

    comments

--- src/index.ts
+++ src/index.ts
@@ -30,18 +30,24 @@ const validateComunidad = (req: express.Request, res: express.Response, next: ex
 
     next();
 };
-//
-app.use(express.json());
+// End of custom validation middlewares
 
+// Express middlewares
+app.use(express.json());
+// End of express middlewares
+
+// Routes
 app.get('/servicio-1', validateComunidad,(req, res) => {
     const comunidades : Comunidad[] = req.body.comunidades;
     console.log(comunidades)
     res.json(req.body);
 });
 
+// End of routes
 
-app.listen(3000, () => {
+// Start server
+app.listen(3000, () => { // TODO: Parametrize port an connection data via .env file (DOES NOT PUSH THE PERSONAL CONFIGURATION TO THE REPO)
     console.log('\x1b[32mServer running on port 3000\x1b[0m');
 });
 
-
+// End of start server
