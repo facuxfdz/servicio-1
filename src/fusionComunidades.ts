594014c7df87800f14081ad1613024a1f20504df
javifgarcia <jagarca@frba.utn.edu.ar>
2023-09-08 00:36:05 -0300

    agregada la funcionalidad de armar el listado a recomendar

--- src/fusionComunidades.ts
+++ src/fusionComunidades.ts
@@ -3,8 +3,9 @@ import Comunidad, { Establecimiento, Servicio } from "./types/Comunidad";
 export function sugerirFusionComunidad (comunidades: Comunidad[]){
 
     const comunidadesCompatibles: Comunidad[][] = [];
+    var comunidadesAuxiliar: Comunidad[][] = [];
 
-
+    //TODO: SE PUEDE HACER UNA TOTAL DE COMUNDIADES DONDE C ADA VE QUE REALIZAMOS UNA PROPUESAT BORRAMOS LA COMUNIDAD DE LA LISTA. ERGO SI NO ESTA EN LA LISTA YA ESTA EN UNA PROPUESTA
     
     for(let i = 0; i < comunidades.length-1; i++){ //[a,b,c,d,e] => [a,b] [b,c] [c,d] [d,e]
         var comunidadAux; //= comunidades[i+1];
@@ -20,16 +21,18 @@ export function sugerirFusionComunidad (comunidades: Comunidad[]){
                 //[a,j,e,f,d] -> Iterar desde j hasta de la lista checkeando que cada elemento sea compatible con el resto.
 
                 //agrupamos las comunidades a la propuestas
-                comunidadesCompatibles.push([comunidades[i], comunidadAux]); // [[a,j],[d,e],[a,e]] => [[a,j,e],[d,e]]
-                
-                //TODO Hacerle un flat a la lista de comunidadesCompatibles aka [[a,j],[a,e],[a,c]] => [[a,j,e],[a,c]]
-                //capaz una lista aoarte que tenga las comuniades iniciales y vas borrando de esa lista a medida que vas agregando a la lista de compatibles
-            }
+                comunidadesAuxiliar.push([comunidades[i], comunidadAux]); // [[a,j],[d,e],[a,e]] => [[a,j,e],[d,f]]
+            
+           }
         }
-
+        
         console.log("Se termino de analizar la comunidad " + comunidades[i].nombre);
     
-
+        //logica de filtrado de comunidades compatibles
+        comunidadesCompatibles.push(armadoDeListaFinal(comunidadesAuxiliar));
+        
+        //borro la lista auxiliar
+        comunidadesAuxiliar = [];
     
     }
 
@@ -45,10 +48,38 @@ export function sugerirFusionComunidad (comunidades: Comunidad[]){
         }
     }
 
-
-
     //retornar las comunidades que estan en la lista de propuestas
     return comunidadesCompatibles;
+}
+//compatibilidades.flat().forEach(comunidad1 => cumpleTodasLasCondiciones(comunidad1, comunaSiguiente));
+function armadoDeListaFinal(compatibilidades: Comunidad[][]){ //[[a,b],[a,c],[a,e]]
+    var comunaSiguiente: Comunidad;
+    var comunaActual: Comunidad;
+    var listadoFinal: Comunidad[] = []; // var? const? ni pta idea
+    const comunaInicial: Comunidad = compatibilidades[0][0]; //now you're thinking with portals!
+
+    listadoFinal.push(comunaInicial);
+
+        for(let i = 0; i < compatibilidades.length-1; i++){ //va -1???
+                                                        ///0     1     2
+                                                    ////[[a,b],[a,c],[a,e]]
+            comunaActual = compatibilidades[i][1]; //[a,b]
+
+            comunaSiguiente = compatibilidades[i+1][1]; //[a,c]
+         
+            if(cumpleTodasLasCondiciones(comunaActual, comunaSiguiente)){
+                listadoFinal.push(comunaActual);
+                listadoFinal.push(comunaSiguiente);
+            }
+        }
+        
+    if(listadoFinal.length == 1){
+        console.log("Solo hay una comunidad para fusionar");
+        listadoFinal.push(compatibilidades[0][1]);//este seria b
+    }
+
+    return listadoFinal;
+            
 }
 
 function cumpleTodasLasCondiciones(comunidad1: Comunidad, comunidad2: Comunidad){
