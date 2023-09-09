df31b2a9b02fcfe80ff75aa31084dba6299402ab
Facundo Fernandez <facuassain@gmail.com>
2023-09-09 04:12:15 -0300

    haciendo la validacion de los 6 meses y no registrando nuevamente propuestas que ya fueron propuestas antes

--- src/fusionComunidades.ts
+++ src/fusionComunidades.ts
@@ -3,6 +3,14 @@ import Comunidad from "./types/Comunidad";
 import PropuestaFusion from "./types/PropuestaFusion"
 import { generarId } from "./utils/generadorIdPropuestas";
 
+interface PropuestaFusionRecord {
+    propuesta: PropuestaFusion;
+    fechaGeneracion: Date;
+}
+
+// Crear un mapa para mantener un registro de las propuestas
+const propuestasRegistradas = new Map<string, PropuestaFusionRecord>();
+
 export function generarPropuestaFusion(comunidades: Comunidad[], criterios: CriterioFusion[]): PropuestaFusion[] {
     const propuestasDeFusion: PropuestaFusion[] = [];
     const comunidadesYaFusionadas = new Set<Comunidad>(); // Para evitar fusionar la misma comunidad más de una vez
@@ -18,14 +26,48 @@ export function generarPropuestaFusion(comunidades: Comunidad[], criterios: Crit
             }
 
             if (comunidadesCompatibles.length > 1) {
-                const idPropuesta = generarId();
-                propuestasDeFusion.push({
-                    id: idPropuesta,
+                const propuesta: PropuestaFusion = {
+                    id: generarId(),
                     comunidadesAFusionar: comunidadesCompatibles,
-                });
+                };
+                const propuestaId = obtenerIdentificadorUnico(comunidadesCompatibles);
+                // Verificar si ya existe una propuesta similar en el registro
+                if (!existePropuestaSimilar(propuestaId)) {
+                    // Agregar la nueva propuesta al registro
+                    const fechaGeneracion = new Date();
+                    propuestasRegistradas.set(propuestaId, { propuesta, fechaGeneracion });
+                    // Agregar la propuesta al resultado
+                    propuestasDeFusion.push(propuesta);
+                }else{
+                    console.log(`Ya existe una propuesta similar a la propuesta ${propuestaId}`);
+                }
             }
         }
     }
 
     return propuestasDeFusion;
+}
+
+function obtenerIdentificadorUnico(comunidades: Comunidad[]): string {
+    // Generar un identificador único basado en las comunidades involucradas
+    const idsComunidades = comunidades.map(comunidad => comunidad.id).sort().join('-');
+    return idsComunidades;
+}
+
+function existePropuestaSimilar(identificadorUnico: string): boolean {
+    // Verificar si existe una propuesta con el mismo identificador único
+    const propuestaRegistrada = propuestasRegistradas.get(identificadorUnico);
+    if (propuestaRegistrada) {
+        // Calcular el tiempo transcurrido desde la generación de la propuesta existente
+        const fechaActual = new Date();
+        const tiempoTranscurrido = fechaActual.getTime() - propuestaRegistrada.fechaGeneracion.getTime();
+
+        // Si han pasado menos de 6 meses, la propuesta es similar y ya existe
+        const seisMesesEnMilisegundos = 6 * 30 * 24 * 60 * 60 * 1000; // Aproximadamente 6 meses
+        if (tiempoTranscurrido < seisMesesEnMilisegundos) {
+            return true;
+        }
+    }
+
+    return false;
 }
